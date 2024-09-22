2e67220654bb136b3577a3b147ddc286e18882b7
Archisman <archismandas1405@gmail.com>
2024-09-22 14:34:51 +0530

    src added

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {PORT} =  require('./config/server.config');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true})); // it is aa middleware that allows parsing of URL endoded bodies
+app.use(bodyParser.text());
+/* extended option allows you to choose between 
+parsing the url encoded data with queryString library(when false) and qs library (when true)*/
+
+app.get('/ping' , (req,res) => {
+    return res.json({message:'Problem Service is alive'});
+});
+
+app.listen(PORT , () => {
+    console.log(`Server started at Port : ${PORT}`);
+});
