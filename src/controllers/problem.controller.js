3eae481529159c545e4b1f612ebe8d16bab9b7ef
Archisman <archismandas1405@gmail.com>
2024-09-23 21:35:46 +0530

    http Status Codes implemented

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,33 +1,35 @@
+const { StatusCodes } = require('http-status-codes');
+
 function pingProblemController(req , res){
     return res.json({message : 'Problem controller up '});
 }
 
 function addProblem(req , res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not implemented'
     });
 }
 
 function getProblem(req , res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not implemented'
     });
 }
 
 function getProblems(req,res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not implemented'
     });
 }
 
 function updateProblems(req,res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not implemented'
     });
 }
 
 function deleteProblems(req,res){
-    return res.status(501).json({
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
         message : 'Not implemented'
     });
 }
@@ -40,3 +42,11 @@ module.exports={
     updateProblems,
     pingProblemController
 }
+
+/*
+
+    res.status -> returns the same response object with status property set
+    .json -> returns the same response object which hs sttus set but this json to 
+             be returned is also set
+
+*/
