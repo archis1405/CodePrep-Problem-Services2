2d4a04221be74f98178afbd73375f76e02e18888
Archisman <archismandas1405@gmail.com>
2024-09-25 21:45:48 +0530

    Feat : added error handler middleware with classes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,13 +1,19 @@
 const { StatusCodes } = require('http-status-codes');
+const NotImplemented = require('../errors/notImplemented.error');
 
-function pingProblemController(req , res){
+function pingProblemController(req , res ){
     return res.json({message : 'Problem controller up '});
 }
 
-function addProblem(req , res){
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message : 'Not implemented'
-    });
+function addProblem(req , res , next){
+    try{
+        // nothing implemented
+        throw new NotImplemented('addProblem');
+    }
+
+    catch(error){
+        next(error); // calling the next middleware with the corresponding error
+    }
 }
 
 function getProblem(req , res){
